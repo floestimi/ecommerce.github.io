f6795fc4c7765199296006a768b718d4e0623466
Florencia <floestimi129@gmail.com>
2023-10-28 23:55:00 -0300

    entrega5

--- js/init.js
+++ js/init.js
@@ -7,14 +7,16 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
+
+// Funcionalidad del carrusel de imágenes 
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
-
 let hideSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
+// Función general para obtener datos JSON
 let getJSONData = function(url){
     let result = {};
     showSpinner();
@@ -39,3 +41,70 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+// Busca la información del usuario en Local storage
+//   Si encuentra la información, pone el nombre de usuario en el navbar
+//   Si no encuentra la información, te redirige a la página de Login
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let usuario = localStorage.getItem('user');
+
+  let usuarioParse = JSON.parse(usuario);
+
+  if (usuario=="" || usuario==null) {
+    this.location.href="login.html";
+  }else{
+    let nombreDeUsuario = usuarioParse.email;
+    document.getElementById('user').innerHTML= nombreDeUsuario.substring(0, nombreDeUsuario.indexOf('@'));
+  } 
+  // Función para cambiar entre el modo claro y oscuro
+function toggleModoOscuro() {
+  // Obtener una referencia al elemento 'body' del documento y los botones
+  const body = document.body;
+  const btnFilrar = document.getElementById('rangeFilterCount');
+  const btnLimpiar = document.getElementById('clearRangeFilter');
+  // Alternar la clase 'dark-mode' en el elemento 'body'
+  body.classList.toggle('dark-mode');
+
+ // Comprobar si la clase 'dark-mode' está presente en 'body' y guardar el estado en localStorage
+  if (body.classList.contains('dark-mode')) {
+      localStorage.setItem('modoOscuro', 'activado');
+      // Agregar una clase específica para el modo oscuro a los botones
+      btnFilrar.classList.add('dark-mode-button-active');
+      btnLimpiar.classList.add('dark-mode-button-active');
+  } else {
+      localStorage.setItem('modoOscuro', 'desactivado');
+      // Remover la clase específica para el modo oscuro de los botones
+      btnFilrar.classList.remove('dark-mode-button-active');
+      btnLimpiar.classList.remove('dark-mode-button-active');
+  }
+}
+
+// Evento que se ejecuta cuando la página se ha cargado completamente
+window.addEventListener('load', () => {
+  // Obtener el estado del modo oscuro guardado en localStorage
+  const modoOscuroGuardado = localStorage.getItem('modoOscuro');
+  // Obtener una referencia al elemento 'body'
+  const body = document.body;
+
+  // Si el modo oscuro está guardado como 'activado', activarlo y marcar la casilla de verificación
+  if (modoOscuroGuardado === 'activado') {
+      body.classList.add('dark-mode'); // Activar el modo oscuro si está guardado
+      document.getElementById('modoOscuroToggle').checked = true; // Marcar la casilla de verificación
+      // Agregar la clase específica para el modo oscuro a los botones
+      document.getElementById('rangeFilterCount').classList.add('dark-mode-button-active');
+      document.getElementById('clearRangeFilter').classList.add('dark-mode-button-active');
+  }
+});
+
+// Agregar un evento de cambio a la casilla de verificación
+const modoOscuroCheckbox = document.getElementById('modoOscuroToggle');
+modoOscuroCheckbox.addEventListener('change', toggleModoOscuro);
+
+
+  //Repito la función para que se cierre sesión al hacer click en Cerrar Sesión
+  document.getElementById("bye").addEventListener('click', function() {
+    localStorage.removeItem('user');
+    location.href="login.html";
+  })
+});
---
+++ js/products.js
@@ -0,0 +1,171 @@
+// Constante con el endpoint incompleto
+// Variable para que pueda usar cualquier id de categoría
+// Constante y variable con la terminación para que quede la URL completa
+const api = "https://japceibal.github.io/emercado-api/cats_products/";
+let catId = localStorage.getItem("catID");
+let apiProducts = api + catId + ".json";
+
+// Variable que contendrá el array de productos
+let products = [];
+
+// Constantes con los inputs de precio mínimo y máximo
+const minCount = document.getElementById("rangeFilterCountMin");
+const maxCount = document.getElementById("rangeFilterCountMax");
+
+// Variable que contendrá el nombre de la categoría de productos
+let catName = "";
+
+// Constante con la ubicación de donde irá la lista de productos
+const contenedorLista = document.getElementById("lista-productos");
+
+// Función que hace fetch a la api
+//   Almacena los productos y el nombre de la categoría
+//   Llama a la función que muestra los productos
+function getProductsData(api) {
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      products = data.products;
+      catName = data.catName;
+    })
+    .then(() => displayProducts(products));
+}
+
+// Función que ordena los productos de forma ascendente o descendente
+//   Crea un nuevo array de productos
+//   Vacía el contenedor de la lista
+function sortProducts(arr, bool, property) {
+
+  let currentProducts = arr;
+  contenedorLista.innerHTML = "";
+  if (bool) {
+    currentProducts.sort((a, b) => a[property] - b[property]);
+    displayProducts(currentProducts);
+  } else {
+    currentProducts.sort((a, b) => b[property] - a[property]);
+    displayProducts(currentProducts);
+  }
+}
+
+// Función que muestra los productos en pantalla
+//   Muestra el nombre de la categoría en pantalla
+function displayProducts(arr) {
+  let divContent = "";
+  for (let i = 0; i < arr.length; i++) {
+    let { soldCount, name, currency, cost, description, image, id} = arr[i];
+    divContent += `    
+    <div onclick="setProductID(${id})" class="contenedor-producto pe-auto bg-light text-dark row"> 
+        <div class="imagen-producto mb-3 col-sm-12 col-md-4 col-lg-4">
+            <img class="img-fluid" src=${image}>
+        </div>
+        <div class="row col-sm-12 col-md-8 col-lg-8 text-center text-md-start text-lg-start text-xg-start">            
+            <div class="col-md-12 col-lg-10">
+                <div class="mb-3">
+                    <h3 class="">${name}</h3>
+                </div>
+                <div class="mb-3">
+                    <h3 class="">${currency} ${cost}</h3>
+                </div>            
+                <div class="">
+                    <p class="">${description} </p> 
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-2 text-center text-md-start text-lg-end text-xg-end">
+                <p class="datos">${soldCount} vendidos</p>
+            </div>
+        </div>
+    </div>
+    `;
+  }
+  contenedorLista.innerHTML = divContent;
+  document.getElementById("nombreCategoria").innerText = catName;
+}
+
+// Función que muestra los productos usando el filtro de precio
+function showProductsInPriceRange() {
+  let minValue = minCount.value || 0;
+  let maxValue = maxCount.value || Math.max(...products.map((e) => e.cost));
+  let currentProducts = products.filter(
+    (e) => e.cost >= minValue && e.cost <= maxValue
+  );
+  displayProducts(currentProducts);
+  return currentProducts;
+}
+
+// Función que cambia la clase Bootstrap para cambiar la imagen de los botones de ordenar productos 
+function changeButtonImage(bool, element, className) {
+  if (bool) {
+    element.classList.remove(`${className}-down`);
+    element.classList.add(`${className}-up`);
+  } else {
+    element.classList.remove(`${className}-up`);
+    element.classList.add(`${className}-down`);
+  }
+}
+
+// Función que agarra el ID de un producto
+//   Lo guarda en Local storage
+//   Redirige a la página del producto seleccionado
+function setProductID(id) {
+  localStorage.setItem("ProductID", id);
+  window.location = "product-info.html";
+}
+
+// Cuando se carga la página
+//   Ejecuta función que hace fetch a la API
+//   Escucha todos los filtros
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBar = document.getElementById("buscar");
+  const sortCost = document.getElementById("precio");
+  const sortRelevancia = document.getElementById("relevancia");
+  const priceSortBtn = document.getElementById("sortPrecio");
+  const relSortBtn = document.getElementById("sortRelevancia");
+  const filterBtn = document.getElementById("rangeFilterCount");
+  const clearFilterBtn = document.getElementById("clearRangeFilter");
+
+  const priceBtnIcon = "fa-sort-numeric";
+  const relBtnIcon = "fa-sort-amount";
+
+  let priceBool = false;
+  let relBool = false;
+
+  getProductsData(apiProducts);
+
+  // Escucha a la barra de busqueda y filtra los productos en tiempo real
+  searchBar.addEventListener("keyup", () => {
+    let arrayProducts = Array.from(
+      document.getElementsByClassName("contenedor-producto")
+    );
+    arrayProducts.forEach((el) => {
+      !el.textContent.toLowerCase().includes(searchBar.value.toLowerCase())
+        ? el.classList.add("buscando")
+        : el.classList.remove("buscando");
+    });
+  });
+
+  // Filtro por precio
+  sortCost.addEventListener("click", () => {
+    priceBool = !priceBool;
+    changeButtonImage(priceBool, priceSortBtn, priceBtnIcon);
+    sortProducts(showProductsInPriceRange(), priceBool, "cost");
+  });
+
+  // Filtro por relevancia (usa cantidad vendidos)
+  sortRelevancia.addEventListener("click", () => {
+    relBool = !relBool;
+    changeButtonImage(relBool, relSortBtn, relBtnIcon);
+    sortProducts(showProductsInPriceRange(), relBool, "soldCount");
+  });
+
+  // Le da funcionalidad al botón de filtro por precio
+  filterBtn.addEventListener("click", () => {
+    showProductsInPriceRange();
+  });
+
+  // Le da funcionalidad al botón que limpia los filtros
+  clearFilterBtn.addEventListener("click", () => {
+    minCount.value = "";
+    maxCount.value = "";
+    displayProducts(products);
+  });
+});
